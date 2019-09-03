c48ce429e1805cf2705edec827da2f652c5c6987
Hugo Osorio <hugo.o0243@gmail.com>
2019-09-02 20:08:50 -0500

    primer prototipo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HabitacionesComponent } from './pages/habitaciones/habitaciones.component';
+
+import {RouterModule, Routes} from '@angular/router';
+import { HabitacionesClienteComponent } from './pages/habitaciones-cliente/habitaciones-cliente.component';
+
+
+const routes: Routes =[
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component : LoginComponent},
+  {path: 'habitaciones', component : HabitacionesComponent},
+  {path: 'cliente/habitaciones', component : HabitacionesClienteComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    HabitacionesComponent,
+    HabitacionesClienteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/habitaciones-cliente/habitaciones-cliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habitaciones-cliente',
+  templateUrl: './habitaciones-cliente.component.html',
+  styleUrls: ['./habitaciones-cliente.component.css']
+})
+export class HabitacionesClienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
