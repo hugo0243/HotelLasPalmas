41c674717e7636d716f5de4b93c19ba310eca388
Hugo A. Osorio <hugo.o0243@gmail.com>
2019-09-15 11:45:14 -0500

    funcionalidad actualización agregada

--- src/app/services/habitacion.service.ts
+++ src/app/services/habitacion.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Habitacion} from '../modelos/Habitacion';
 import {Observable, of} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
+/* import {map} from 'rxjs/operators'; */
 
 @Injectable({
   providedIn: 'root'
